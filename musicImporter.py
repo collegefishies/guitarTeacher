7afaf05e461c772c6c871be21df44af51238c59b
Enrique Mendez <eqm@mit.edu>
2022-04-30 18:13:47 -0400

    Add musicImporter skeleton.

---
+++ musicImporter.py
@@ -0,0 +1,12 @@
+'''
+	A single class that takes in a guitar tab file and outputs a single format of *unique* note/chord identifiers over time.
+'''
+
+class musicImporter():
+	self.convertedMusic = None
+
+	def returnMusic():
+		return self.convertedMusic
+		
+	def importMIDI(self, midi):
+		pass
