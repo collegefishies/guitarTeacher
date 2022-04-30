a393e064bb35b8614fbf6d29767be7572132b634
Enrique Mendez <eqm@mit.edu>
2022-04-29 20:58:14 -0400

    Add title.

--- guitarTeacher.py
+++ guitarTeacher.py
@@ -2,8 +2,25 @@ import sys
 import PyQt5
 from PyQt5.QtWidgets import *
 
-class guitarMainWindow(QWidget):
-	pass
+class guitarMainWindow(QMainWindow):
+
+	def __init__(self):
+		super(guitarMainWindow,self).__init__()
+		#define primitive widgets
+		self.setGeometry(100,100,400,300)
+		self.mainPage = QWidget()
+		self.mainLayout = QVBoxLayout()
+		self.mainPage.setLayout(self.mainLayout)
+		self.setCentralWidget(self.mainPage)
+
+		#start up code
+		self.updateTitle()
+		
+	def updateTitle(self):
+		self.setWindowTitle('Guitar Tab Player')
+
+	def _addToolBar(self):
+		pass
 
 def main():
 	app = QApplication(sys.argv)
